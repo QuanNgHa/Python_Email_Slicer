a0fa0a99bd398b0bbb8913e6c04d17001e074ec5
CodeXploreRePo <codexplore.channel@gmail.com>
2020-06-27 17:47:28 +0800

    Email Slicer

---
+++ main_module.py
@@ -0,0 +1,12 @@
+from codexplore import emailProcess, printMsg
+
+
+def main():
+    emails = ['qyd@gmail.com', 'youtube@codexplore.dev', 'liverpool@winner.com']
+    for email in emails:
+        username, email_domain = emailProcess(email)
+        printMsg(username, email_domain)
+
+
+if __name__ == "__main__":
+    main()
